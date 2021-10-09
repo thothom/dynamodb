9bd7503d63daf2a92e9db7bb75d0eae15b7e43a2
Henrique Leite <henriqueleite616@gmail.com>
2021-10-09 10:00:35 -0300

    Add Basic Config (#2)

--- src/lib/connection/index.ts
+++ src/lib/connection/index.ts
@@ -1,13 +1,29 @@
-import { Connection } from "@techmmunity/symbiosis";
+import { BaseConnectionOptions, Connection } from "@techmmunity/symbiosis";
+import { DynamoDBClient, DynamoDBClientConfig } from "@aws-sdk/client-dynamodb";
 import { ExampleRepository } from "../repository";
 import { ColumnExtraMetadata } from "../types/column-extra-metadata";
 import { EntityExtraMetadata } from "../types/entity-extra-metadata";
 
 export class ExampleConnection extends Connection<
+	DynamoDBClientConfig,
 	EntityExtraMetadata,
 	ColumnExtraMetadata
 > {
+	private readonly connectionInstance: DynamoDBClient;
+
+	public constructor(options: BaseConnectionOptions<DynamoDBClientConfig>) {
+		super(options);
+
+		this.connectionInstance = new DynamoDBClient(
+			options.databaseConnectionConfig || {},
+		);
+	}
+
 	public getRepository<Entity>(entity: Entity) {
-		return new ExampleRepository(entity);
+		return new ExampleRepository(
+			this.connectionInstance,
+			this.entityManager,
+			entity,
+		);
 	}
 }
