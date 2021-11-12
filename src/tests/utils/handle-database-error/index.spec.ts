69b775e96c89c83e0bee186c33709ce1282a8c22
Henrique Leite <henriqueleite616@gmail.com>
2021-11-12 15:21:29 -0300

    Improve Error Handling (#62)

--- src/tests/utils/handle-database-error/index.spec.ts
+++ src/tests/utils/handle-database-error/index.spec.ts
@@ -1,7 +1,13 @@
 import { SymbiosisError } from "@techmmunity/symbiosis";
 import { handleDatabaseError } from "../../../lib/utils/handle-database-error";
 
+const ERROR_MESSAGE = "FAILED, SHOULD RETURN NOT THROW";
+
 describe("handleDatabaseError", () => {
+	const context = {
+		tableName: "foo_bar",
+	};
+
 	describe("With SymbiosisError", () => {
 		it("should return the same error", () => {
 			let result: any;
@@ -14,12 +20,35 @@ describe("handleDatabaseError", () => {
 			});
 
 			try {
-				result = handleDatabaseError(error);
+				result = handleDatabaseError(context as any, error);
 			} catch (err: any) {
-				result = "FAILED, SHOULD RETURN NOT THROW";
+				result = ERROR_MESSAGE;
 			}
 
 			expect(result).toStrictEqual(error);
+		});
+	});
+
+	describe("With table not found", () => {
+		it("should return an error", () => {
+			let result: any;
+
+			const error = new Error("Requested resource not found");
+
+			try {
+				result = handleDatabaseError(context as any, error);
+			} catch (err: any) {
+				result = ERROR_MESSAGE;
+			}
+
+			expect(result).toStrictEqual(
+				new SymbiosisError({
+					code: "OPERATION_FAILED",
+					origin: "DATABASE",
+					message: "Table not found",
+					details: [`Table "${context.tableName}" not found.`],
+				}),
+			);
 		});
 	});
 
@@ -28,9 +57,9 @@ describe("handleDatabaseError", () => {
 			let result: any;
 
 			try {
-				result = handleDatabaseError(new Error("foo"));
+				result = handleDatabaseError(context as any, new Error("foo"));
 			} catch (err: any) {
-				result = "FAILED, SHOULD RETURN NOT THROW";
+				result = ERROR_MESSAGE;
 			}
 
 			expect(result).toStrictEqual(
